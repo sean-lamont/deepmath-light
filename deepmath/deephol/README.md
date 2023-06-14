81cf65cf12cd064bf7b10fe392674cb71542fdd8
sean-lamont <sean.a.lamont@outlook.com>
2023-06-14 17:15:55 +0930

    Initial commit

--- deepmath/deephol/README.md
+++
@@ -1,3 +0,0 @@
-This directory contains code related to DeepHOL.
-
-Please see our website http://deephol.org for more details.
--- deepmath/zz/build_all.sh
+++
@@ -1,3 +0,0 @@
-MODULES=":ZZ_Prelude :ZZ_Generics :ZZ_CmdLine :ZZ_Console :ZZ_Unix :ZZ_CodeBreeder :ZZ_HolLight"
-BUILD/zb r $MODULES
-
